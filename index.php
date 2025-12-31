73a2a0790595ce9a27b3e3361b8e4c89a65d5b6c
Isaac <isaacuchoa@gmail.com>
2025-12-31 05:49:27 -0300

    site datai

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>datai.com.br</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" type="image/x-icon" href="images/fav.png">
+		
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="logo"><img src="images/logo.png" alt="" />					
+				</div>
+				
+			</header>
+
+		<!-- Footer -->
+			<footer id="footer">
+				
+						
+					
+					<ul class="copyright">
+						<li>&copy; datai.com.br | 2025</li>
+					</ul>
+				
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
